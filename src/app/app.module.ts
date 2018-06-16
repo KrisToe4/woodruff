49a7a962a8d8646afcb4654521af8d9c34a86de9
Kris Wilson <kris.wilson@mindbodyonline.com>
2018-06-16 10:46:18 -0700

    Switch to SCSS and added some initial content/cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WoodruffComponent } from './woodruff/woodruff.component';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButt
     MatCardModule,
     MatMenuModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/woodruff/woodruff.component.ts
+++ src/app/woodruff/woodruff.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'woodruff',
   templateUrl: './woodruff.component.html',
-  styleUrls: ['./woodruff.component.css']
+  styleUrls: ['./woodruff.component.scss']
 })
 export class WoodruffComponent {
   cards = [
-    { title: 'Card 1', cols: 2, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 3', cols: 1, rows: 2 },
-    { title: 'Card 4', cols: 1, rows: 1 }
+    { title: 'Header', cols: 2, rows: 1 },
+    { title: 'Menu', cols: 2, rows: 1 },
+    { title: 'Content', cols: 1, rows: 3 },
+    { title: 'Bio', cols: 1, rows: 1 },
+    { title: 'Classes', cols: 1, rows: 1 },
+    { title: 'Quote', cols: 1, rows: 1 }
   ];
 }
