ac207e71d2bade5c1d400b297905566ca7388b06
Kris Wilson <kris.wilson@mindbodyonline.com>
2018-06-17 14:40:29 -0700

    Sidebar cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WoodruffComponent } from './woodruff/woodruff.component';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatListModule } from '@angular/material';
+import { BioComponent } from './bio/bio.component';
+import { ClassListComponent } from './class-list/class-list.component';
+import { QuoteOfTheDayComponent } from './quote-of-the-day/quote-of-the-day.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WoodruffComponent
+    WoodruffComponent,
+    BioComponent,
+    ClassListComponent,
+    QuoteOfTheDayComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +24,7 @@ import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButt
     MatCardModule,
     MatMenuModule,
     MatIconModule,
+    MatListModule,
     MatButtonModule,
     MatToolbarModule
   ],
--- src/app/woodruff/woodruff.component.ts
+++ src/app/woodruff/woodruff.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BioComponent } from '../bio/bio.component';
 
 @Component({
   selector: 'woodruff',
@@ -7,9 +8,9 @@ import { Component } from '@angular/core';
 })
 export class WoodruffComponent {
   cards = [
-    { title: 'Content', cols: 1, rows: 3 },
-    { title: 'Bio', cols: 1, rows: 1 },
-    { title: 'Classes', cols: 1, rows: 1 },
-    { title: 'Quote', cols: 1, rows: 1 }
+    { title: 'Content', cols: 3, rows: 3, background: 'white' },
+    { title: 'Bio', cols: 1, rows: 1, background: 'transparent'},
+    { title: 'ClassList', cols: 1, rows: 1, background: 'transparent'},
+    { title: 'QuoteOfTheDay', cols: 1, rows: 1, background: 'transparent'}
   ];
 }
