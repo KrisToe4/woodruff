82af2e605fdd50ca1ca660eb4532c3fbb194b45d
Kris Wilson <kris.wilson@mindbodyonline.com>
2018-06-17 10:52:41 -0700

    Work on layout and UI

--- src/app/woodruff/woodruff.component.ts
+++ src/app/woodruff/woodruff.component.ts
@@ -7,8 +7,6 @@ import { Component } from '@angular/core';
 })
 export class WoodruffComponent {
   cards = [
-    { title: 'Header', cols: 2, rows: 1 },
-    { title: 'Menu', cols: 2, rows: 1 },
     { title: 'Content', cols: 1, rows: 3 },
     { title: 'Bio', cols: 1, rows: 1 },
     { title: 'Classes', cols: 1, rows: 1 },
